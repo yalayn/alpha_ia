1d6e999d555af6557e951bc62e2628db12242ed5
Yordin Da Rocha <y.alayn@gmail.com>
2026-05-06 20:11:12 -0400

    feat: configure global validation pipes and domain exception filters in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { DomainExceptionFilter } from './infrastructure/filters/domain-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
   
+  // Validaciones globales
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  // Filtro de excepciones global
+  app.useGlobalFilters(new DomainExceptionFilter());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   
